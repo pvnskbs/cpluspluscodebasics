fd2582026f1aa22723a0a4ca11e6771603b51629
pvnskbs <47807623+pvnskbs@users.noreply.github.com>
2026-02-22 21:59:17 +0530

    Update swap_wo_3rdvar.cc

--- src/swap_wo_3rdvar.cc
+++ src/swap_wo_3rdvar.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,11 +7,13 @@ int main() {
 
     int a = 10, b = 20;
     int *p = &a, *q = &b;    // pointer
+    cout << "Before swap = " << *p << ", " << *q << endl;
+
     *p = *p + *q;
     *q = *p - *q;
     *p = *p - *q;
     
-    cout << *p << ", " << *q << endl;
+    cout << "After swap = " << *p << ", " << *q << endl;
 
     return 0;
 }
